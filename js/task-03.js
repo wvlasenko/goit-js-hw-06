8030b1f90686f6d133f4c226ae0fd98fe6b736d9
wvlasenko <44614847+wvlasenko@users.noreply.github.com>
2020-08-11 20:29:58 +0300

    Add tas 03;04;05.

--- js/task-03.js
+++ js/task-03.js
@@ -1,1 +1,10 @@
 "use strict";
+import users from './users.js'
+
+const getUsersWithGender = (users, gender) => {
+    const userWithGender = users.filter(user => user.gender === gender).map(user => user.name)
+    return userWithGender
+};
+
+console.log(getUsersWithGender(users, 'male'));
+// [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,11 @@
 "use strict";
+import users from './users.js'
+
+const getInactiveUsers = users => {
+    const inactiveUser = users.filter(user => !user.isActive)
+    return inactiveUser;
+
+};
+
+console.log(getInactiveUsers(users));
+// [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
