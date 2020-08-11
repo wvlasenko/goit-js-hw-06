77bfa091ab9eead309ac23f1160e2863de6629b8
wvlasenko <44614847+wvlasenko@users.noreply.github.com>
2020-08-11 20:47:55 +0300

    Add task 06 and task 07

--- js/task-06.js
+++ js/task-06.js
@@ -1,1 +1,14 @@
 "use strict";
+import users from './users.js'
+
+const getUsersWithAge = (users, min, max) => {
+    const userWithAge = users.filter(user => user.age > min && user.age < max)
+    return userWithAge;
+
+};
+
+console.log(getUsersWithAge(users, 20, 30));
+// [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
+
+console.log(getUsersWithAge(users, 30, 40));
+// [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
---
+++ js/task-07.js
@@ -0,0 +1,8 @@
+"use strict";
+import users from './users.js'
+const calculateTotalBalance = users => {
+   const totalBalance = users.reduce((acc,user)=> acc+user.balance,0)
+    return totalBalance;
+};
+
+console.log(calculateTotalBalance(users)); // 20916
