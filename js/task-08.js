6c193c6957575564d38d476e24c5492109b89954
wvlasenko <44614847+wvlasenko@users.noreply.github.com>
2020-08-18 12:55:22 +0300

    Fix by menthor comment

--- js/task-08.js
+++ js/task-08.js
@@ -1,14 +1,21 @@
-"use strict";
-import users from './users.js'
+'use strict';
+import users from './users.js';
 
+// const getUsersWithFriend = (users, friendName) => {
+//   const userWithFriend = users.reduce((acc, user) => {
+//     if (user.friends.includes(friendName)) {
+//       acc.push(user.name);
+//     }
+//     return acc;
+//   }, []);
+//   return userWithFriend;
+// };
 const getUsersWithFriend = (users, friendName) => {
-    const userWithFriend = users.reduce((acc, user) => {
-        if (user.friends.includes(friendName)) {
-            acc.push(user.name);
-        }
-        return acc;
-    },[]);
-    return userWithFriend;
+  const userWithFriend = users.filter(user =>
+    user.friends.includes(friendName),
+  );
+
+  return userWithFriend.map(user => user.name);
 };
 
 console.log(getUsersWithFriend(users, 'Briana Decker'));
